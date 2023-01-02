6e97f3d0a8e763ec174274705fb84b7e3e8f6212
Аlexandr <mirage@MBA.local>
2023-01-02 12:48:03 +0700

    Add terragrunt

--- variables.tf
+++ variables.tf
@@ -3,22 +3,3 @@ variable "region" {
   type        = string
 }
 
-variable "image" {
-  description = "container image you will run on the instance"
-  type        = string
-}
-
-variable "instance_ami" {
-  description = "AWS AMI"
-  type        = string
-}
-
-variable "instance_user" {
-  description = "instance user"
-  type        = string
-}
-
-variable "env" {
-  description = "environment"
-  type        = string
-}
