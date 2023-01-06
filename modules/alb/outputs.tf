8d927ebbe77a5a8fcb9c046dd94aaf9ea4a583bf
Аlexandr <mirage@MBA.local>
2023-01-06 13:55:52 +0700

    Add dashboard

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -6,3 +6,11 @@ output "tg_arn" {
   value = aws_alb_target_group.main.arn
 }
 
+output "alb_arn_suffix" {
+  value = aws_lb.main.arn_suffix
+}
+
+output "tg_arn_suffix" {
+  value = aws_alb_target_group.main.arn_suffix
+}
+
---
+++ modules/dashboard/variables.tf
@@ -0,0 +1,27 @@
+variable "name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "target_group_arn" {
+  type = string
+}
+
+variable "alb_arn" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+variable "service_name" {
+  type = string
+}
+
